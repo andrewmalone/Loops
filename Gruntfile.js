cd540706620542852d1e5ac4eed0943c03ced46e
andrewmalone <andrew@andrewmalone.com>
2014-08-09 08:11:20 -0400

    Reorganizing
    Based on http://www.thomasboyt.com/2013/09/01/maintainable-grunt.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,99 +1,29 @@
 module.exports = function(grunt) {
-
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-		    dist: {
-		      options: {
-			    style: 'compressed'  
-		      },
-		      files: [{
-		        expand: true,
-		        cwd: 'css',
-		        src: ['*.scss'],
-		        dest: 'dest/',
-		        ext: '.css'
-		      }]
-		    }
-		},
-        copy: {
-	        main: {
-		        src: ['*.html', '!_*'],
-		        dest: 'dest/'
-	        }
-        }
-		/*
-shell: {
-            jekyllBuild: {
-                command: 'jekyll build --drafts'
-            },
-            jekyllPublish: {
-	            command: 'jekyll build'
-            }
-        },
-		connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    base: '_site'
-                }
-            }
-        },
-		watch: {
-          livereload: {
-            files: [
-                '_config.yml',
-                'index.html',
-                '_layouts/**',
-                '_posts/**',
-                '_drafts/**',
-                '_includes/**',
-                'css/**'
-            ],
-            tasks: ['sass', 'autoprefixer', 'shell:jekyllBuild'],
-            options: {
-              livereload: true
-            },
-          },
-        },
-        sass: {
-		    dist: {
-		      options: {
-			    style: 'compressed'  
-		      },
-		      files: {
-		        'css/main.css': 'css/main.scss'
-		      }
-		    }
-		},
-		autoprefixer: {
-			options: {
-				browsers: ['last 2 version']
-			},
-			single_file: {
-				src: 'css/main.css',
-				dest: 'css/main_prefixed.css'
-			}
-		}
-*/
-    });
-
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    //grunt.loadNpmTasks('grunt-contrib-concat');
-    //grunt.loadNpmTasks('grunt-contrib-uglify');
-/*
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-*/
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-sass');    
-
+	// Utility to load the different option files
+	// based on their names
+	function loadConfig(path) {
+		var glob = require('glob');
+		var object = {};
+		var key;
+		
+		glob.sync('*', {cwd: path}).forEach(function(option) {
+			key = option.replace(/\.js$/,'');
+			object[key] = require(path + option);
+		});
+	
+		return object;
+	}
+	grunt.loadTasks('tasks');
+    var config = {
+    	pkg: grunt.file.readJSON('package.json')
+	}
+	
+	grunt.util._.extend(config, loadConfig('./tasks/options/'));
+	grunt.initConfig(config);
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-   // grunt.registerTask('default', ['sass', 'autoprefixer', 'shell:jekyllBuild', 'connect', 'watch']);
+    require('load-grunt-tasks')(grunt);
+	grunt.registerTask('default', ['copy']);
+	
 	//grunt.registerTask('publish', ['sass', 'autoprefixer', 'shell:jekyllPublish']);
 
 };
